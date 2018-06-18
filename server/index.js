cd0f03d76182ed4edbf9e39655e74865f4e7a48d
Connor Jessup <cjessup@umich.edu>
2018-06-18 19:37:25 -0400

    created base app

--- server/index.js
+++ server/index.js
@@ -3,11 +3,51 @@ const express = require('express')
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 const compression = require('compression')
-const session = require('express-session')
-const passport = require('passport')
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
 const db = require('./db')
-const sessionStore = new SequelizeStore({db})
 const PORT = process.env.PORT || 8080
 const app = express()
 module.exports = app
+
+// logging middleware
+app.use(morgan('dev'))
+
+// body parsing middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// compression middleware
+app.use(compression())
+
+// auth and api routes
+app.use('/auth', require('./auth'))
+app.use('/api', require('./api'))
+
+// static file-serving middleware
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+// any remaining requests with an extension (.js, .css, etc.) send 404
+app.use((req, res, next) => {
+  if (path.extname(req.path).length) {
+    const err = new Error('Not found')
+    err.status = 404
+    next(err)
+  } else {
+    next()
+  }
+})
+
+// sends index.html
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+})
+
+// error handling endware
+app.use((err, req, res, next) => {
+  console.error(err)
+  console.error(err.stack)
+  res.status(err.status || 500).send(err.message || 'Internal server error.')
+})
+
+app.listen(PORT, () => console.log(`Server online at port ${PORT}`))
+  .then(db.sync())
+  .catch(err => console.error(err))
